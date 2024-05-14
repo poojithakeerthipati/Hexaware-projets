2faf9da06ecfaaf40364c53589d03171857d24bc
poojithakeerthipati <poojikeerthipati123@gmail.com>
2024-05-14 15:39:31 +0530

    Dicttionary comprehension

--- day_14_comprehensions.py
+++ day_14_comprehensions.py
@@ -12,3 +12,9 @@ print(unique_colors_1)
 words = ["This", "is", "cool", "mangoes", "oranges", "apple"]
 words_count = {len(word) for word in words}
 print(words_count)
+
+
+# Dictionary comprehension
+
+squares = {x: x**2 for x in range(1, 21)}
+print(squares)
