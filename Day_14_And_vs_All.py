edf777c3360eee28ba0051144a9a7da33c8ddcaa
poojithakeerthipati <poojikeerthipati123@gmail.com>
2024-05-14 17:43:45 +0530

    And vs All

---
+++ Day_14_And_vs_All.py
@@ -0,0 +1,2 @@
+print(all([True, False, True]))  # acts as And
+print(any([True, False, True]))  # acts as or
--- Day_14_nested_loops.py
+++ Day_14_nested_loops.py
@@ -21,39 +21,76 @@ classes = {
 #     "Class B": {"Dave": 92.50, "Eve": 86.50, "Frank": 950},
 # }
 
-# result = {}
-# for class_name, students in classes.items():
-#     student_average = {}
-#     for student in students:
-#         average_grade = sum(student["grades"]) / len(student["grades"])
-#         student_average[student["name"]] = round(average_grade, 2)
-#     result[class_name] = student_average
-
-# pprint(result)
-
 
 # average_marks = {
 #     student["grades"]: round(sum(student["grades"]) / len(student["grades"]), 2)
 #     for student in students
 # }
-def calculate_student_avg(classes):
-    result = {}
-    for class_name, students in classes.items():
-        # print(class_name)
-        # print(students)
-        class_student_avg = {}
-        total_marks = []
-        final_result = {}
-        count = 0
-        for student in students:
-            average_grade = sum(student["grades"]) / len(student["grades"])
-            total_marks.append(round(average_grade, 2))
-            count += 1
-            class_student_avg[student["name"]] = round(average_grade, 2)
-            result[class_name] = class_student_avg
-        final_result[class_name] = round(sum(total_marks) / count, 2)
-        print(final_result)
-    return result
+# def calculate_student_avg(classes):
+#     result = {}
+#     for class_name, students in classes.items():
+#         # print(class_name)
+#         # print(students)
+#         class_student_avg = {}
+#         total_marks = []
+#         final_result = {}
+#         count = 0
+#         for student in students:
+#             average_grade = sum(student["grades"]) / len(student["grades"])
+#             total_marks.append(round(average_grade, 2))
+#             count += 1
+#             class_student_avg[student["name"]] = round(average_grade, 2)
+#             result[class_name] = class_student_avg
+#         final_result[class_name] = round(sum(total_marks) / count, 2)
+#         print(final_result)
+#     return result
 
 
-pprint(calculate_student_avg(classes))
+# pprint(calculate_student_avg(classes))
+
+
+def find_avg(items):
+    return round(sum(items) / len(items), 2)
+
+
+# result = {}
+# for class_name, students in classes.items():
+#     student_average = {}
+#     for student in students:
+#         average_grade = find_avg(student["grades"])
+#         student_average[student["name"]] = average_grade
+#     result[class_name] = student_average
+
+# pprint(result)
+
+# result = {}
+# for class_name, students in classes.items():
+#     student_average = {
+#         student["name"]: find_avg(student["grades"]) for student in students
+#     }
+#     result[class_name] = student_average
+# pprint(result)
+
+
+# class_avg = {}
+# for class_name, students in classes.items():
+#     class_student_avg = [find_avg(student["grades"]) for student in students]
+#     class_avg[class_name] = find_avg(class_student_avg)
+# print(class_avg)
+
+# class_avg = {
+#     class_name: find_avg([find_avg(student["grades"]) for student in students])
+#     for class_name, students in classes.items()
+# }
+
+# print(class_avg)
+
+
+result = {}
+for class_name, students in classes.items():
+    student_average = {
+        student["name"]: find_avg(student["grades"]) for student in students
+    }
+    result[class_name] = student_average
+
+pprint(result)
