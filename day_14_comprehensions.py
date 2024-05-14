09af0667f336701598847fd254d156e06b063b7f
poojithakeerthipati <poojikeerthipati123@gmail.com>
2024-05-14 15:35:55 +0530

    set comprehension examples

---
+++ day_14_comprehensions.py
@@ -0,0 +1,14 @@
+colors = ["red", "blue", "red", "green", "pink", "blue"]
+
+unique_colors = set()
+for color in colors:
+    unique_colors.add(color)
+print(unique_colors)
+
+unique_colors_1 = {clr for clr in colors}
+print(unique_colors_1)
+
+
+words = ["This", "is", "cool", "mangoes", "oranges", "apple"]
+words_count = {len(word) for word in words}
+print(words_count)
